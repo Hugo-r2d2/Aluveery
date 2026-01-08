36544db8a01808a63d3c9dc99d1aae6affdd4377
Hugo-r2d2 <ihugocs@gmail.com>
2026-01-08 17:49:25 -0400

    ajst

--- app/src/main/java/br/com/alura/curso/aluvery/ui/theme/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/curso/aluvery/ui/theme/screens/HomeScreen.kt
@@ -1,12 +1,14 @@
 package br.com.alura.curso.aluvery.ui.theme.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,21 +23,24 @@ fun HomeScreen(
     sections: Map<String, List<Product>>
 ) {
     Scaffold { paddingValues ->
-        LazyColumn(
-            Modifier
-                .fillMaxSize()
-                .padding(paddingValues),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            contentPadding = PaddingValues(vertical = 16.dp)
-        ) {
-            for (section in sections) {
-                val title = section.key
-                val products = section.value
-                item {
-                    ProductsSection(
-                        title = title,
-                        products = products,
-                    )
+        Column {
+            TextField(value = "alex", onValueChange = {newValue -> })
+            LazyColumn(
+                Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                contentPadding = PaddingValues(vertical = 16.dp)
+            ) {
+                for (section in sections) {
+                    val title = section.key
+                    val products = section.value
+                    item {
+                        ProductsSection(
+                            title = title,
+                            products = products,
+                        )
+                    }
                 }
             }
         }
